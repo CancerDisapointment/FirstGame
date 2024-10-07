c0915ef11f67d1a4ff700c00ff7a8bd16caaa442
CancerDisapointment <dcompanion192@gmail.com>
2024-10-07 17:08:49 -0400

    Goes insane

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    public Rigidbody2D rb;
+    
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            rb.velocity = Vector3.up;
+        if(Input.GetKey(KeyCode.A))
+           rb.velocity = Vector3.left;
+        if(Input.GetKey(KeyCode.D))
+            rb.velocity = Vector3.right;
+           
+           
+
+    }
+}
